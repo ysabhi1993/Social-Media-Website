ac0fb822c03b2444d9c90c6bd8f670ec786f756c
Abhishek S YELLANKI <abhishekys1993@gmail.com>
2017-08-23 12:20:28 -0400

    Instantiate Memcached Servers/help

---
+++ use_memcache.php
@@ -0,0 +1,12 @@
+<?php
+
+//Create an object of Memcached
+$memcache = new Memcached();
+//Create 4 instances 
+$MEMCACHED_SERVERS = array('127.0.0.1', '127.0.0.2', '127.0.0.3','127.0.0.4')
+foreach($MEMCACHED_SERVERS as $server){
+	$memcache->addServer($server, 11211);
+	$memcache->setOption(Memcached::OPT_COMPRESSION, false);
+}
+
+?>
