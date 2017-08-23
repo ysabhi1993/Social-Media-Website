c61ef97f9afe6463ba2dbb1f639eef448046e1a9
Abhishek S YELLANKI <abhishekys1993@gmail.com>
2017-08-23 14:06:42 -0400

    Memcached Instantiated

--- Memcached/use_memcache.php
+++ Memcached/use_memcache.php
@@ -7,7 +7,7 @@ try{
     	die($e->getMessage());
 }
 //Create 4 instances 
-$MEMCACHED_SERVERS = array('127.0.0.1', '127.0.0.2', '127.0.0.3','127.0.0.4')
+$MEMCACHED_SERVERS = array('127.0.0.1', '127.0.0.2', '127.0.0.3','127.0.0.4');
 foreach($MEMCACHED_SERVERS as $server){
 	$memcache->addServer($server, 11211);
 	$memcache->setOption(Memcached::OPT_COMPRESSION, false);
